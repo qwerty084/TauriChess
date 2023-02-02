729252f1a614b3b18d01b92acc369e31b05f7fdc
Luca Hendrik Helms <lucahendrik.dev@gmail.com>
2023-02-02 21:38:18 +0100

    replace fetch, with tauri http client

--- src/components/profile/ChessComProfile.vue
+++ src/components/profile/ChessComProfile.vue
@@ -1,20 +1,48 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { defineAsyncComponent, ref } from 'vue';
 import type { ChessComUser } from '@/types/ChessComApi';
+import { getClient } from '@tauri-apps/api/http';
+import { CheckCircleIcon } from '@heroicons/vue/24/outline';
+import { NotificationType } from '@/types/Types';
 
 const username = ref('');
 const userData = ref<ChessComUser | null>(null);
+const showDialog = ref(false);
+const notificationType = ref<NotificationType>();
+const notificationTitle = ref('');
+const NotificationDialog = defineAsyncComponent(
+  () => import('../ui/NotificationDialog.vue')
+);
 
 async function fetchProfile() {
   if (username.value === '') {
     console.warn('Username is empty');
   }
 
-  const data = await fetch(
+  const client = await getClient();
+  const res = await client.get<ChessComUser>(
     `https://api.chess.com/pub/player/${username.value}`
   );
-  userData.value = await data.json();
-  console.log(userData.value);
+
+  if (res.ok) {
+    userData.value = res.data;
+    notificationType.value = 'Success';
+    notificationTitle.value = 'Your Chess.com account has been added!';
+  } else {
+    if (res.status === 404) {
+      notificationType.value = 'Error';
+      notificationTitle.value = 'Your Chess.com account could not be found!';
+    } else {
+      notificationType.value = 'Error';
+      notificationTitle.value = 'Your Chess.com account could not be added!';
+    }
+  }
+
+  showDialog.value = true;
+
+  setTimeout(() => {
+    showDialog.value = false;
+  }, 3500);
 }
 </script>
 
@@ -27,22 +55,30 @@ async function fetchProfile() {
           class="block text-sm font-medium text-blue-gray-900"
           >Username</label
         >
-        <input
-          id="username"
-          v-model="username"
-          type="text"
-          class="mt-1 block w-full rounded-md border-gray-300 text-blue-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-        />
+        <div class="relative mt-1 rounded-md shadow-sm">
+          <input
+            id="username"
+            v-model="username"
+            type="text"
+            class="mt-1 block w-full rounded-md border-gray-300 text-blue-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+          />
+          <div
+            class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3"
+          >
+            <CheckCircleIcon
+              v-if="userData != null"
+              class="w-6 h-6 text-green-600 block"
+            />
+          </div>
+        </div>
       </div>
     </div>
 
+    <NotificationDialog v-if="showDialog" :notification-type="notificationType"
+      ><template #title>{{ notificationTitle }}</template></NotificationDialog
+    >
+
     <div class="flex justify-end pt-8">
-      <button
-        type="button"
-        class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-blue-gray-900 shadow-sm hover:bg-blue-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-      >
-        Cancel
-      </button>
       <button
         type="submit"
         class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
@@ -50,6 +86,17 @@ async function fetchProfile() {
       >
         Save
       </button>
+    </div>
+    <div
+      v-if="userData"
+      class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow"
+    >
+      <div class="px-4 py-5 sm:px-6">
+        <h2>{{ userData.username }}</h2>
+      </div>
+      <div class="px-4 py-5 sm:p-6">
+        <!-- Content goes here -->
+      </div>
     </div>
   </div>
 </template>
--- src/components/profile/LichessProfile.vue
+++ src/components/profile/LichessProfile.vue
@@ -1,19 +1,48 @@
 <script setup lang="ts">
 import { LichessUser } from '@/types/LichessApi';
-import { ref } from 'vue';
+import { defineAsyncComponent, ref } from 'vue';
+import { getClient } from '@tauri-apps/api/http';
+import { CheckCircleIcon } from '@heroicons/vue/24/outline';
+import { NotificationType } from '@/types/Types';
 
 const username = ref('');
 const userData = ref<LichessUser | null>(null);
+const showDialog = ref(false);
+const notificationType = ref<NotificationType>();
+const notificationTitle = ref('');
+const NotificationDialog = defineAsyncComponent(
+  () => import('../ui/NotificationDialog.vue')
+);
 
 async function fetchProfile() {
   if (username.value === '') {
     console.warn('Username is empty');
   }
 
-  const res = await fetch(`https://lichess.org/api/user/${username.value}`);
-  userData.value = await res.json();
+  const client = await getClient();
+  const res = await client.get<LichessUser>(
+    `https://lichess.org/api/user/${username.value}`
+  );
 
-  console.log(userData.value);
+  if (res.ok) {
+    userData.value = res.data;
+    notificationType.value = 'Success';
+    notificationTitle.value = 'Your Lichess account has been added!';
+  } else {
+    if (res.status === 404) {
+      notificationType.value = 'Error';
+      notificationTitle.value = 'Your Lichess account could not be found!';
+    } else {
+      notificationType.value = 'Error';
+      notificationTitle.value = 'Your Lichess account could not be added!';
+    }
+  }
+
+  showDialog.value = true;
+
+  setTimeout(() => {
+    showDialog.value = false;
+  }, 3500);
 }
 </script>
 
@@ -26,22 +55,30 @@ async function fetchProfile() {
           class="block text-sm font-medium text-blue-gray-900"
           >Username</label
         >
-        <input
-          id="username"
-          v-model="username"
-          type="text"
-          class="mt-1 block w-full rounded-md border-gray-300 text-blue-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-        />
+        <div class="relative mt-1 rounded-md shadow-sm">
+          <input
+            id="username"
+            v-model="username"
+            type="text"
+            class="mt-1 block w-full rounded-md border-gray-300 text-blue-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+          />
+          <div
+            class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3"
+          >
+            <CheckCircleIcon
+              v-if="userData != null"
+              class="w-6 h-6 text-green-600 block"
+            />
+          </div>
+        </div>
       </div>
     </div>
 
+    <NotificationDialog v-if="showDialog" :notification-type="notificationType"
+      ><template #title>{{ notificationTitle }}</template></NotificationDialog
+    >
+
     <div class="flex justify-end pt-8">
-      <button
-        type="button"
-        class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-blue-gray-900 shadow-sm hover:bg-blue-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-      >
-        Cancel
-      </button>
       <button
         type="submit"
         class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
@@ -49,6 +86,17 @@ async function fetchProfile() {
       >
         Save
       </button>
+    </div>
+    <div
+      v-if="userData"
+      class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow"
+    >
+      <div class="px-4 py-5 sm:px-6">
+        <h2>{{ userData.username }}</h2>
+      </div>
+      <div class="px-4 py-5 sm:p-6">
+        <!-- Content goes here -->
+      </div>
     </div>
   </div>
 </template>
